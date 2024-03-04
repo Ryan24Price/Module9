00a064f25f513a737ee32fa8eeb1fa8bab8b3ba6
Ryan24Price <illusion165@hotmail.com>
2024-03-04 01:28:45 -0500

    found the misspelling issue and corrected it

--- mySQLBlog/controllers/index.js
+++ mySQLBlog/controllers/index.js
@@ -2,5 +2,6 @@
 module.exports = {
     userController: require('./userControllers'),
     postController: require('./postController'),
-    commentController: require('./commentController')
+    commentController: require('./commentControllers')
 }
+
